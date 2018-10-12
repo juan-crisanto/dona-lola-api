346d2ffc2ef1d53756d3555280c40d997a932a29
Luis Martinez <luismartinez@outlook.com.pe>
2018-10-11 23:33:34 -0500

    [FEAT] se agregan clases util

---
+++ donalola-core/src/main/java/com/donalola/util/localization/GeoLocationUtil.java
@@ -0,0 +1,7 @@
+package com.donalola.util.localization;
+
+import com.donalola.commons.GeoLocation;
+
+public interface GeoLocationUtil {
+    double getDistanceBetween(GeoLocation from, GeoLocation to);
+}
