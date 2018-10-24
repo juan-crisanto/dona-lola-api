81d33b4a6564995b3299fd868821b35b90d1d24c
Luis Martinez <lmartinezh@proveedor.intercorp.com.pe>
2018-10-24 10:34:37 -0500

    cambio a genericos en servicio de footplace

--- donalola-food-places/src/main/java/com/donalola/foodplaces/service/FoodPlaceService.java
+++ donalola-food-places/src/main/java/com/donalola/foodplaces/service/FoodPlaceService.java
@@ -1,11 +1,12 @@
 package com.donalola.foodplaces.service;
 
+
 import com.donalola.core.service.BaseService;
 import com.donalola.foodplaces.dto.FoodPlaceRequestDto;
 import com.donalola.foodplaces.dto.FoodPlaceResponseDto;
 
-public interface FoodPlaceService extends BaseService {
+public interface FoodPlaceService<T extends FoodPlaceRequestDto> extends BaseService {
 
-    FoodPlaceResponseDto proceed(FoodPlaceRequestDto foodPlaceRequestDto);
+    FoodPlaceResponseDto proceed(T foodPlaceRequestDto);
 
 }
--- donalola-food-places/src/main/java/com/donalola/foodplaces/service/SaveFoodPlaceServiceImpl.java
+++ donalola-food-places/src/main/java/com/donalola/foodplaces/service/SaveFoodPlaceServiceImpl.java
@@ -1,6 +1,7 @@
 package com.donalola.foodplaces.service;
 
 
+import com.donalola.foodplaces.FoodPlace;
 import com.donalola.foodplaces.dao.entity.FoodPlaceEntity;
 import com.donalola.foodplaces.dao.repository.FoodPlaceRepository;
 import com.donalola.foodplaces.dto.FindNearbyFoodPlacesRequestDto;
@@ -12,26 +13,24 @@ import java.util.List;
 
 @Slf4j
 @Service
-public class FindNearbyFoodPlacesServiceImpl implements FoodPlaceService<FindNearbyFoodPlacesRequestDto> {
+public class SaveFoodPlaceServiceImpl implements FoodPlaceService<FindNearbyFoodPlacesRequestDto> {
 
     private final FoodPlaceRepository       foodPlaceRepository;
     private final FoodPlacesLocationService foodPlacesLocationService;
 
-    public FindNearbyFoodPlacesServiceImpl(FoodPlaceRepository foodPlaceRepository, FoodPlacesLocationService foodPlacesLocationService){
+    public SaveFoodPlaceServiceImpl(FoodPlaceRepository foodPlaceRepository, FoodPlacesLocationService foodPlacesLocationService){
         this.foodPlaceRepository = foodPlaceRepository;
         this.foodPlacesLocationService = foodPlacesLocationService;
     }
 
     @Override
     public FoodPlaceResponseDto proceed(FindNearbyFoodPlacesRequestDto request){
-
         List<FoodPlaceEntity> foodPlaceEntityList = foodPlaceRepository.findAll();
-
         return foodPlacesLocationService.getNearbyPlaces(request, foodPlaceEntityList);
     }
 
     @Override
     public boolean supports(Class<?> supportedClass){
-        return FindNearbyFoodPlacesRequestDto.class.isAssignableFrom(supportedClass);
+        return FoodPlace.class.isAssignableFrom(supportedClass);
     }
 }
